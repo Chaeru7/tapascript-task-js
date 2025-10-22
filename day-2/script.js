0ccfa4574771ae367c2c165c9a3962338d682779
Chaeru7 <heru.asp7@gmail.com>
2025-10-22 12:29:48 +0800

    task day 2

---
+++ day-2/script.js
@@ -0,0 +1,26 @@
+
+const name = "Raven";
+const age = 25;
+const student_status = true;
+let programming_language = "JavaScript";
+
+console.log(`
+    name = ${name}
+    age = ${age}   
+    status = ${student_status}
+    programming language = ${programming_language}`
+);
+
+
+/* name = 'jhon';
+programming_language = 'Python';
+
+console.log(`
+    name = ${name}
+    programming language = ${programming_language}`
+); */
+
+
+const array = [1, 2, 3, 4, 5];
+array.push(6);
+console.log(array);
