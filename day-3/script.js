8387ed9c3fab60fdcaee63da7d109f6e65ead141
Chaeru7 <heru.asp7@gmail.com>
2025-11-04 19:40:31 +0800

    task 4

--- day-3/script.js
+++ day-3/script.js
@@ -1,5 +1,5 @@
 // task 1
-let number = 5;
+let number = 2;
 const kindOfNumber = (number % 2) ? 'odd' : 'even';
 console.log(`The number ${number} is ${kindOfNumber}.`);
 
@@ -12,9 +12,12 @@ console.log(`A person who is ${age} years old is ${d_license} for a driving lice
 console.log(`A person who is ${age} years old is ${driving_license} for a driving license.`); */
 
 //task 3
-let salary = 12300;
-let salari_with_bonus = salary + (salary * 0.2);
-console.log(`The salary after 20% bonus is: ${salari_with_bonus}`);
+const monthly_salary = 12300;
+const annual_salary = monthly_salary * 12;
+const bonus_annual = 0.2;
+const final_salary = annual_salary + (annual_salary * bonus_annual);
+
+console.log(`The salary after 20% bonus is: ${final_salary}`);
 
 //task 4
 let color = "green";
@@ -34,21 +37,39 @@ console.log(`The total annual payment after 20% discount is: ${total_annual_paym
 //task 6
 /*
 📘 Leap Year Rules
-
 In the Gregorian calendar, a year is considered a leap year if it meets the following conditions:
-
 ✅ 1️⃣ If the year is divisible by 4 → it might be a leap year
-
 But it’s not automatically a leap year yet.
 We need to check the next conditions.
-
 🚫 2️⃣ If the year is also divisible by 100 → it is not a leap year
-
 Example:
 The year 1900 is divisible by 100 → ❌ not a leap year.
-
 ✅ 3️⃣ Unless the year is also divisible by 400 → then it is a leap year
-
 Example:
 The year 2000 is divisible by 400 → ✅ leap year.
 */
+
+const year = 2000;
+const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? 'leap year' : 'not a leap year';
+console.log(`The year ${year} is ${isLeapYear}`);
+
+//task 7
+let p = 6;
+let q = 200;
+let r = 30;
+
+// (p terbesar?) ? p : (q terbesar?) ? q : r
+let max = (p >= q && p >= r) ? p : (q >= p && q >= r) ? q : r;
+
+console.log(`maximum of these three numbers is ${max}.`);
+
+//task 8
+
+const count = 5;
+
+let doubledCount = count << 1;
+
+console.log(`before : ${count}`);
+console.log(`after bitwise doubling (count << 1): ${doubledCount}`);
+
+
